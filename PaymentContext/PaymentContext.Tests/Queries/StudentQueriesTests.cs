b40a76bfe25697581f0e7ea43fad9a00dc3f2e26
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-01-24 21:45:08 -0300

    PaymentContext: Testes de queries

---
+++ PaymentContext/PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            _students = new List<Student>();
+            for (var i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    name: new Name("Aluno", "Sobrenome"),
+                    document: new Document("1111111111" + i.ToString(), EDocumentType.CPF),
+                    email: new Email(i.ToString() + "@teste.net")
+                ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldRetornNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("1234567891651");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, studn);
+        }
+
+        [TestMethod]
+        public void ShouldRetornStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("11111111111");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreNotEqual(null, studn);
+        }
+    }
+}
